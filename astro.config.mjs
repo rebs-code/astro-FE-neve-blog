18a9e154895d336eb0defad31100cfb021813c8a
rebs-code <jack.rebussi@gmail.com>
2024-05-21 11:31:37 +0200

    feat: Add Tailwind CSS configuration and update dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,42 +1,39 @@
-import { fileURLToPath } from 'url'
-import path, { dirname } from 'path'
-import { defineConfig } from 'astro/config'
-import { sanityIntegration } from '@sanity/astro'
-import react from '@astrojs/react'
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import { defineConfig } from 'astro/config';
+import { sanityIntegration } from '@sanity/astro';
+import react from '@astrojs/react';
+import tailwind from "@astrojs/tailwind";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        resolve: {
-            alias: {
-                '@/': `${path.resolve(__dirname, 'src')}/`
-            }
-        },
-        css: {
-            preprocessorOptions: {
-                scss: {
-                    // path to your scss variables
-                    //additionalData: `@use "./src/styles/style.scss";`,
-                }
-            }
+  vite: {
+    resolve: {
+      alias: {
+        '@/': `${path.resolve(__dirname, 'src')}/`
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          // path to your scss variables
+          //additionalData: `@use "./src/styles/style.scss";`,
         }
-    },
-    image: {
-        remotePatterns: [
-            {
-                protocol: 'https'
-            }
-        ]
-    },
-    integrations: [
-        sanityIntegration({
-            projectId: 'bno79kb2',
-            dataset: 'production',
-            apiVersion: '2024-05-20',
-            useCdn: false
-        }),
-        react()
-    ]
-})
+      }
+    }
+  },
+  image: {
+    remotePatterns: [{
+      protocol: 'https'
+    }]
+  },
+  integrations: [sanityIntegration({
+    projectId: 'bno79kb2',
+    dataset: 'production',
+    apiVersion: '2024-05-20',
+    useCdn: false
+  }), react(), tailwind()]
+});
