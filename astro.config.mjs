af72e569c68e51528e901d79995abe30630dc948
rebs-code <jack.rebussi@gmail.com>
2024-05-21 11:19:01 +0200

    feat: import bootstrap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,42 @@
-import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'url'
+import path, { dirname } from 'path'
+import { defineConfig } from 'astro/config'
+import { sanityIntegration } from '@sanity/astro'
+import react from '@astrojs/react'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        resolve: {
+            alias: {
+                '@/': `${path.resolve(__dirname, 'src')}/`
+            }
+        },
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    // path to your scss variables
+                    //additionalData: `@use "./src/styles/style.scss";`,
+                }
+            }
+        }
+    },
+    image: {
+        remotePatterns: [
+            {
+                protocol: 'https'
+            }
+        ]
+    },
+    integrations: [
+        sanityIntegration({
+            projectId: 'bno79kb2',
+            dataset: 'production',
+            apiVersion: '2024-05-20',
+            useCdn: false
+        }),
+        react()
+    ]
+})
