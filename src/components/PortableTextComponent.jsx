3650d16b0836c89b923c912e807f8d480bdcfe33
rebs-code <jack.rebussi@gmail.com>
2024-05-21 11:23:53 +0200

    add: portabletextcomponent

---
+++ src/components/PortableTextComponent.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { urlForImage } from "../library/urlForImage";
+
+export const myPortableTextComponents = {
+  types: {
+    image: ({ value }) => {
+      const imageUrl = urlForImage(value).format("webp").url();
+      return <img src={`${imageUrl}`} alt="test alt" />;
+    },
+    list: (node) => {
+      return (
+        <ul>
+          {node.children.map((child, index) => (
+            <li key={index}>{child}</li>
+          ))}
+        </ul>
+      );
+    },
+    divider: ({ value }) => {
+      return <div style={{ height: `${value.height}px` }} />;
+    },
+  },
+
+  marks: {
+    span: ({ children }) => <sup className="sup">{children}</sup>,
+    link: (node) => {
+      return (
+        <a href={node.value.link} target={node.value._blank ? "_blank" : null}>
+          {node.children}
+        </a>
+      );
+    },
+  },
+};
