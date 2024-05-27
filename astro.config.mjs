f8ca195f35a9355a868233b06a575772fcbf5027
rebs-code <jack.rebussi@gmail.com>
2024-05-27 11:52:33 +0200

    feat: Update dependencies and add @astrojs/netlify package

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,14 @@ import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+import node from '@astrojs/node';
 
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: netlify(),
   vite: {
     resolve: {
       alias: {
@@ -37,6 +40,5 @@ export default defineConfig({
     apiVersion: '2024-05-21',
     useCdn: false
   }), react(), tailwind()],
-
-  site: 'https://www.my-site.dev', //add here final site domain to unlock more astro features and RSS feed
+  site: 'https://www.my-site.dev' //add here final site domain to unlock more astro features and RSS feed
 });
